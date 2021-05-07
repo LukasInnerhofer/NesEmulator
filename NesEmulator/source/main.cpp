5270b6ffa2a21462d323bf8a1af02e7d8149dab1
Lukas Innerhofer <lukas.ihofer@gmail.com>
2021-05-07 22:46:16 +0200

    Added command line arg for ines file.

--- NesEmulator/source/main.cpp
+++ NesEmulator/source/main.cpp
@@ -5,12 +5,18 @@
 
 #include "nes.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	if(argc < 2)
+	{
+		return EXIT_SUCCESS;
+	}
+	std::string filePath { argv[1] };
+
 	Nes nes;
 
 	std::ifstream file;
-	file.open(R"(LibMos6502\test\test.nes)", std::ios::in | std::ios::binary | std::ios::ate);
+	file.open(filePath, std::ios::in | std::ios::binary | std::ios::ate);
 
 	if(!file.good())
 	{
