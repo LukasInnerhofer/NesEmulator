418bcaf229cbc1df61aee63196030c83bb7ce0e0
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-11-07 21:20:04 +0100

    Fixed indentation

--- libmos6502/include/libmos6502/memory.h
+++ libmos6502/include/libmos6502/memory.h
@@ -7,9 +7,9 @@ namespace LibMos6502
 
 class Memory
 {
-	public:
-		virtual uint8_t read(uint16_t addr) = 0;
-		virtual void write(uint16_t addr, uint8_t data) = 0;
+public:
+	virtual uint8_t read(uint16_t addr) = 0;
+	virtual void write(uint16_t addr, uint8_t data) = 0;
 };
 
 }
